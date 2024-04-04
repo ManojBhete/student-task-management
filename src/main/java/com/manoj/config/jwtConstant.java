d3a6583b871976fa6859dfb05e3c31b62eecbe18
manoj <manojbhete76@gmail.com>
2024-04-05 04:16:55 +0530

    User model Plus spring security

---
+++ src/main/java/com/manoj/config/jwtConstant.java
@@ -0,0 +1,7 @@
+package com.manoj.config;
+
+public class jwtConstant {
+
+    public static final String SECRET_KEY ="ewdcedcdcxxwedwejdjbjajhaxxsdhjabashxbashxbsahxhsx";
+    public static final String JWT_HEADER ="Authorization";
+}
---
+++ src/main/java/com/manoj/model/User.java
@@ -0,0 +1,26 @@
+package com.manoj.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Longid;
+    private String email;
+    private String role;
+    private String fullName;
+
+
+
+
+
+}
